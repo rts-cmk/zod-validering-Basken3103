3f39a2f22bc9c09d2763321bfd5ee3fb6c81380e
Christopher S. Vesterby <vesterby@hotmail.dk>
2026-01-09 13:49:53 +0100

    Det jeg nåede af opgaven.

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1,10 +1,35 @@
+import z from 'zod';
+import RegistrationSchema from '../schemas/registrationSchema';
 import './RegistrationForm.sass';
+import { useState } from 'react';
 
 export default function RegistrationForm() {
 
-    // const der sikre at projektet ikke hele tiden reloader, imens man loader
+    // State der tjekker efter fejl
+    const [errors, setErrors] = useState({});
+
+
+    
     const submitHandler = event => {
         event.preventDefault();
+
+        const form = event.target;
+        const formData = new FormData(form);
+        const formDataObject = Object.fromEntries(formData.entries());
+
+        const result = RegistrationSchema.safeParse(formDataObject);
+
+        if(result.success) {
+            setErrors({});
+            alert('Du er nu registreret som bruger!');
+        } else {
+            const readableErrors = z.treeifyError(result.error)
+            console.log(readableErrors.properties);
+
+            setErrors(readableErrors.properties);
+            
+        }
+
 
         
     }
@@ -17,38 +42,66 @@ export default function RegistrationForm() {
             <legend className="registration-form__legend">Personlige oplysninger</legend>
             <label className="registration-form__label">
                 <span className="registration-form__text">Fornavn</span>
-                <input type="text" name="firstName" className="registration-form__input" />
-                <ul className="registration-form__error-list"></ul>
+                <input type="text" name="firstName" className="registration-form__input" autoComplete="given-name" />
+                <ul className="registration-form__error-list">
+                    {errors.firstName?.errors.map(
+                        (message, index) => <li key={index}>{message}</li>
+                    )}
+                </ul>
             </label>
             <label className="registration-form__label">
                 <span className="registration-form__text">Efternavn</span>
-                <input type="text" name="lastName" className="registration-form__input" />
-                <ul className="registration-form__error-list"></ul>
+                <input type="text" name="lastName" className="registration-form__input" autoComplete="family-name" />
+                <ul className="registration-form__error-list">
+                     {errors.lastName?.errors.map(
+                        (message, index) => <li key={index}>{message}</li>
+                    )}
+                </ul>
             </label>
             <label className="registration-form__label">
                 <span className="registration-form__text">Email</span>
-                <input type="email" name="email" className="registration-form__input" />
-                <ul className="registration-form__error-list"></ul>
+                <input type="email" name="email" className="registration-form__input" autoComplete="email"/>
+                <ul className="registration-form__error-list">
+                     {errors.email?.errors.map(
+                        (message, index) => <li key={index}>{message}</li>
+                    )}
+                </ul>
             </label>
             <label className="registration-form__label">
                 <span className="registration-form__text">Password</span>
-                <input type="password" name="password" className="registration-form__input" />
-                <ul className="registration-form__error-list"></ul>
+                <input type="password" name="password" className="registration-form__input" autoComplete="new-password" />
+                <ul className="registration-form__error-list">
+                     {errors.password?.errors.map(
+                        (message, index) => <li key={index}>{message}</li>
+                    )}
+                </ul>
             </label>
             <label className="registration-form__label">
                 <span className="registration-form__text">Gentag password</span>
-                <input type="password" name="confirm password" className="registration-form__input" />
-                <ul className="registration-form__error-list"></ul>
+                <input type="password" name="confirm password" className="registration-form__input" autoComplete="new-password" />
+                <ul className="registration-form__error-list">
+                     {errors.password?.errors.map(
+                        (message, index) => <li key={index}>{message}</li>
+                    )}
+                </ul>
             </label>
             <label className="registration-form__label">
                 <span className="registration-form__text">Fødselsdato</span>
-                <input type="text" name="birthdate" className="registration-form__input" />
-                <ul className="registration-form__error-list"></ul>
+                <input type="text" name="birthdate" className="registration-form__input" autoComplete="birthdate"/>
+                <ul className="registration-form__error-list">
+                     {errors.birthdate?.errors.map(
+                        (message, index) => <li key={index}>{message}</li>
+                    )}
+                </ul>
             </label>
             <label className="registration-form__label">
                 <span className="registration-form__text">Telefonnummer</span>
-                <input type="tel" name="phone" className="registration-form__input" />
-                <ul className="registration-form__error-list"></ul>
+                <input type="tel" name="phone" className="registration-form__input" autoComplete="tel" />
+                <ul className="registration-form__error-list">
+                     {errors.phone?.errors.map(
+                        (message, index) => <li key={index}>{message}</li>
+                    )}
+                </ul>
             </label>
         </fieldset>
 
--- src/schemas/registrationSchema.js
+++ src/schemas/registrationSchema.js
@@ -1,13 +1,13 @@
 import z from "zod";
 
 const RegistrationSchema = z.object({
-    firstName: z.string().min(3),
-    lastName: z.string().min(3),
-    email: z.email(),
-    password: z.string().min(8),
+    firstName: z.string().nonempty('Fornavn skal udfyldes'),
+    lastName: z.string().nonempty('Efternavn skal udfyldes'),
+    email: z.email('Din email addresse skal være gyldig'),
+    password: z.string().min(8, 'Din adgangskode skal være på mindst 8 tegn'),
     confirmPassWord: z.string().nonempty(),
-    phone: z.number(),
-    birthdate: birthdateSchema
+    phone: z.coerce.number('Dit telefonnummer må kun indeholde tal'),
+    birthdate: z.coerce.number('Du skal indtaste din fødselsdato'),
 }); 
 
-export default RegistrationSchema
+export default RegistrationSchema;
